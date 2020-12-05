c34f3d3cf0aa3808146edae471b5ef8b41500f76
priya2707 <kanagaraj.rathnapriya@gmail.com>
2020-12-05 13:19:14 +0100

    First commit

---
+++ src/test/java/appium.java
@@ -0,0 +1,11 @@
+public class appium {
+
+    public static void main(String[] args) {
+        int sum, a=3, b=5;
+        System.out.println("Calculation starts...");
+        sum = a + b;
+        System.out.println(sum);
+
+    }
+
+}
