cabb275e66766cfcebe3c62478d82f4994bc3ac3
priya2707 <kanagaraj.rathnapriya@gmail.com>
2020-12-05 14:12:16 +0100

    New Sum

--- src/test/java/appium.java
+++ src/test/java/appium.java
@@ -1,11 +1,12 @@
 public class appium {
 
     public static void main(String[] args) {
-        int sum, a=3, b=5;
+        int sum, a=3, b=5, c=6;
         System.out.println("Calculation starts...");
         sum = a + b;
         System.out.println(sum);
         System.out.println("Hi");
+        int sum2=sum+c;
     }
 
 }
